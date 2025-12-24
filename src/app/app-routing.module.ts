abd88b5edb027a4e8fd6a30ea7f6378e7bd57e8d
Dinesh10 <dineshiam95@gmail.com>
2025-12-24 13:47:08 +0530

    Fixed build error1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import {  NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { RouterModule, Routes } from '@angular/router';
